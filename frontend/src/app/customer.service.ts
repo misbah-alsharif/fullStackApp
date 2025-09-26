79ffb06a9331ed71ff44ae0b041b475c4c764285
misbah <misbahalshary8@gmail.com>
2025-09-27 01:29:12 +0300

    Update customer.service.ts

--- frontend/src/app/customer.service.ts
+++ frontend/src/app/customer.service.ts
@@ -14,7 +14,7 @@ export interface Customer {
 
 export class CustomerService {
 
-  private apiUrl = 'http://localhost:8080/customers';
+  private apiUrl = 'http://18.193.109.0:30100/customers';
 
   constructor(private http: HttpClient) {}
 
